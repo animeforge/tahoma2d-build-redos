ef76776f746650f9b3e6d5b9593435a10ee1f88d
Aleksandr Borisov <sashaborisov.20032005@gmail.com>
2025-02-12 15:48:36 +0700

    Refactoring for directory layout (place all working files in "build" directory)

--- tahoma2d_builder.sh
+++ tahoma2d_builder.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
-cd $(dirname "$0")
+echo ">>> Create build"
+if ! [ -d $(dirname "$0")/build ]
+then
+	mkdir $(dirname "$0")/build
+fi
 
+HOME_PATH=$(dirname "$0")/build
+cd $HOME_PATH
 if ! [ "$EUID" -ne 0 ];
 then 
-	if ! [ -d "./tahoma2d" ];
-	then
+	if ! [ -d $HOME_PATH/tahoma2d ];
+	then	
+		echo ">>> Download Tahoma2D repo"
 		git clone https://github.com/tahoma2d/tahoma2d.git
 		chmod -R ugo+rwx tahoma2d
 	else
-		echo "directory is exists"
+		echo ">>> Directory is exists"
 	fi
 	
+	echo ">>> Install packages .rpm"
 	dnf install -y gcc gcc-c++ automake git cmake boost boost-devel SuperLU SuperLU-devel lz4-devel lzma lzo-devel libjpeg-turbo-devel libGLEW glew-devel freeglut-devel freeglut freetype-devel libpng-devel qt5-qtbase-devel qt5-qtsvg qt5-qtsvg-devel qt5-qtscript qt5-qtscript-devel qt5-qttools qt5-qttools-devel qt5-qtmultimedia-devel blas blas-devel json-c-devel libtool intltool make qt5-qtmultimedia libmypaint-devel qt5-qtserialport-devel-5.15.1-1.el7.x86_64 qt5-qttools-static-5.15.1-6.el7.x86_64 turbojpeg-devel-2.1.5-1.el7.x86_64 libdeflate-devel-1.19-1.el7.x86_64 
 	
 	echo ">>> Copu OpenCV.rpm"
-	
+	if ! [ -d $HOME_PATH/OpenCV ]
+	then
+		mkdir $HOME_PATH/OpenCV
+	fi
 	for item in opencv-4.6.0-7.el7.x86_64.rpm opencv-contrib-4.6.0-7.el7.x86_64.rpm opencv-core-4.6.0-7.el7.x86_64.rpm opencv-devel-4.6.0-7.el7.x86_64.rpm;
 	do 
-		if ! [ -e /opencv/$item ];
+		if ! [ -e $HOME_PATH/OpenCV/$item ];
 		then
-			cd opencv/
+			cd $HOME_PATH/OpenCV
 			wget https://github.com/animeforge/tahoma2d-build-redos/releases/download/v1.5-beta-2024-10-27/$item
 			cd ../
 		fi
-	done				
+	done
 	
-	dnf install -y $(dirname "$0")/opencv/opencv-4.6.0-7.el7.x86_64.rpm $(dirname "$0")/opencv/opencv-contrib-4.6.0-7.el7.x86_64.rpm $(dirname "$0")/opencv/opencv-core-4.6.0-7.el7.x86_64.rpm $(dirname "$0")/opencv/opencv-devel-4.6.0-7.el7.x86_64.rpm
+	echo">>> Install OpenCV.rpm"
+	dnf install -y $HOME_PATH/OpenCV/opencv-4.6.0-7.el7.x86_64.rpm $HOME_PATH/OpenCV/opencv-contrib-4.6.0-7.el7.x86_64.rpm $HOME_PATH/OpenCV/opencv-core-4.6.0-7.el7.x86_64.rpm $HOME_PATH/OpenCV/opencv-devel-4.6.0-7.el7.x86_64.rpm
 	
-	
+	echo ">>> Tahoma2D assembly"
 	cd tahoma2d/thirdparty/tiff-*
 	./configure --with-pic --disable-jbig --disable-webp 
 	make -j6
@@ -39,69 +51,26 @@ then
 	cmake ../sources
 	make -j6
 	
-	
-	
 	source /opt/qt515/bin/qt515-env.sh
+	
 	echo ">>> Temporary install of Tahoma2D"
-	SCRIPTPATH=`dirname "$0"`
-	export BUILDDIR=$SCRIPTPATH tahoma2d/toonz/build
+	export BUILDDIR=$HOME_PATH/tahoma2d/toonz/build
 	cd $BUILDDIR
-
 	ldconfig
 	
-	
 	echo ">>> Creating appDir"
-	if [ -d appdir ]
+	if [ -d $HOME_PATH/appdir ]
 	then
-	   rm -rf appdir
+	   rm -rf $HOME_PATH/appdir
 	fi
-	mkdir -p appdir/usr
+	mkdir -p $HOME_PATH/appdir/usr
 
 	echo ">>> Copy and configure Tahoma2D installation in appDir"
-	cp -r /opt/tahoma2d/* appdir/usr
-	cp appdir/usr/share/applications/*.desktop appdir
-	cp appdir/usr/share/icons/hicolor/*/apps/*.png appdir
-	mv appdir/usr/lib/tahoma2d/* appdir/usr/lib
-	rmdir appdir/usr/lib/tahoma2d
-
-#	echo ">>> Creating Tahoma2D directory"
-#	if [ -d Tahoma2D ]
-#	then
-#	   rm -rf Tahoma2D
-#	fi
-#	mkdir Tahoma2D
-
-#	echo ">>> Copying stuff to Tahoma2D/tahomastuff"
-
-#	mv appdir/usr/share/tahoma2d/stuff Tahoma2D/tahomastuff
-#	chmod -R 777 Tahoma2D/tahomastuff
-#	rmdir appdir/usr/share/tahoma2d
-#
-#	find Tahoma2D/tahomastuff -name .gitkeep -exec rm -f {} \;
-
-#	if [ -d ../../thirdparty/apps/ffmpeg/bin ]
-#	then
-#	   echo ">>> Copying FFmpeg to Tahoma2D/ffmpeg"
-#	   if [ -d Tahoma2D/ffmpeg ]
-#	   then
-#		  rm -rf Tahoma2D/ffmpeg
-#	   fi
-#	   mkdir -p Tahoma2D/ffmpeg
-#	   cp -R ../../thirdparty/apps/ffmpeg/bin/ffmpeg ../../thirdparty/apps/ffmpeg/bin/ffprobe Tahoma2D/ffmpeg
-#	   chmod -R 755 Tahoma2D/ffmpeg
-#	fi
-
-#	if [ -d ../../thirdparty/apps/rhubarb ]
-#	then
-#	   echo ">>> Copying Rhubarb Lip Sync to Tahoma2D/rhubarb"
-#	   if [ -d Tahoma2D/rhubarb ]
-#	   then
-#		  rm -rf Tahoma2D/rhubarb
-#	   fi
-#	   mkdir -p Tahoma2D/rhubarb
-#	   cp -R ../../thirdparty/apps/rhubarb/rhubarb ../../thirdparty/apps/rhubarb/res Tahoma2D/rhubarb
-#	   chmod 755 -R Tahoma2D/rhubarb
-#	fi
+	cp -r /opt/tahoma2d/* $HOME_PATH/appdir/usr
+	cp $HOME_PATH/appdir/usr/share/applications/*.desktop $HOME_PATH/appdir
+	cp $HOME_PATH/appdir/usr/share/icons/hicolor/*/apps/*.png $HOME_PATH/appdir
+	mv $HOME_PATH/appdir/usr/lib/tahoma2d/* $HOME_PATH/appdir/usr/lib
+	rmdir $HOME_PATH/appdir/usr/lib/tahoma2d
 
 	if [ -d ../../thirdparty/canon/Library ]
 	then
@@ -110,16 +79,16 @@ then
 	fi
 
 	echo ">>> Copying libghoto2 supporting directories"
-	cp -r /usr/local/lib/libgphoto2 appdir/usr/lib
-	cp -r /usr/local/lib/libgphoto2_port appdir/usr/lib
+	cp -r /usr/local/lib/libgphoto2 $HOME_PATH/appdir/usr/lib
+	cp -r /usr/local/lib/libgphoto2_port $HOME_PATH/appdir/usr/lib
 
-	rm appdir/usr/lib/libgphoto2/print-camera-list
-	find appdir/usr/lib/libgphoto2* -name *.la -exec rm -f {} \;
-	find appdir/usr/lib/libgphoto2* -name *.so -exec patchelf --set-rpath '$ORIGIN/../..' {} \;
+	rm $HOME_PATH/appdir/usr/lib/libgphoto2/print-camera-list
+	find $HOME_PATH/appdir/usr/lib/libgphoto2* -name *.la -exec rm -f {} \;
+	find $HOME_PATH/appdir/usr/lib/libgphoto2* -name *.so -exec patchelf --set-rpath '$ORIGIN/../..' {} \;
 
-	cd $(dirname "$0")
-	cp -f scripts/tahoma2d appdir/usr/bin/
-	cd $BUILDDIR
+	cd $HOME_PATH/../
+	cp -f tahoma2d $HOME_PATH/appdir/usr/bin/
+	cd $HOME_PATH
 	
 	echo ">>> Creating Tahoma2D/Tahoma2D.AppImage"
 
@@ -129,8 +98,8 @@ then
 	   chmod a+x linuxdeployqt*.AppImage
 	fi
 
-	export LD_LIBRARY_PATH=appdir/usr/lib/tahoma2d
-	./linuxdeployqt*.AppImage appdir/usr/bin/tahoma2d -bundle-non-qt-libs -verbose=0 -always-overwrite -no-strip \
+	export LD_LIBRARY_PATH=appdir/usr/lib
+	./linuxdeployqt*.AppImage appdir/usr/bin/Tahoma2D -bundle-non-qt-libs -verbose=0 -always-overwrite -no-strip \
 	   -executable=appdir/usr/bin/lzocompress \
 	   -executable=appdir/usr/bin/lzodecompress \
 	   -executable=appdir/usr/bin/tcleanup \
@@ -138,18 +107,16 @@ then
 	   -executable=appdir/usr/bin/tconverter \
 	   -executable=appdir/usr/bin/tfarmcontroller \
 	   -executable=appdir/usr/bin/tfarmserver 
-	   
+	
+	rm appdir/AppRun
+	cp $HOME_PATH/../scripts/AppRun appdir
+	chmod 775 $HOME_PATH/appdir/AppRun
+
 
 	./linuxdeployqt*.AppImage appdir/usr/bin/Tahoma2D -appimage -no-strip 
 
-#	mv Tahoma2D*.AppImage Tahoma2D/Tahoma2D.AppImage
-
-#	echo ">>> Creating Tahoma2D Linux package"
-
-#	tar zcf Tahoma2D-linux.tar.gz Tahoma2D
-	cd $BUILDDIR
-	chmod -R ugo+rwx ../tahoma2d-build-redos
+	chmod -R ugo+rwx $HOME_PATH
+	echo ">>> Start app Tahoma2D:"$HOME_PATH/Tahoma2D-x86_64.AppImage
 else
-	echo "run script from root"
-exit
+	echo ">>> Run script from root"
 fi
